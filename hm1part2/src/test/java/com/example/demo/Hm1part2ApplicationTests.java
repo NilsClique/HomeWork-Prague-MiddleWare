d3787ee18a92756a1f9b429508b5ba31ce8d5293
NilsClique <35067559+NilsClique@users.noreply.github.com>
2022-07-19 11:30:51 +0200

    Add files via upload

---
+++ hm1part2/src/test/java/com/example/demo/Hm1part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hm1part2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
